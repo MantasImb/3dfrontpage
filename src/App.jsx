e036cc112fb2d41b3758d63853fc71c2f6cdd10e
Mantas Imbrasas <mantas@Mantass-Air.lan>
2024-11-08 18:25:32 +0200

    migration to vite, add tailwindcss and an overlay component

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import ThreeCanvas from "./ThreeCanvas"
+import Overlay from "./Overlay"
+
+export default function App() {
+  return (
+    <>
+      <ThreeCanvas />
+      <Overlay />
+    </>
+  )
+}
---
+++ src/Overlay.jsx
@@ -0,0 +1,10 @@
+export default function Overlay() {
+  return (
+    <div className="h-screen w-screen absolute top-0 left-0 pointer-events-none">
+      <h1>Hello World</h1>
+      <a href="https://github.com/mantas-ie/3dfrontpage" target="_blank" rel="noreferrer" className="pointer-events-auto">
+        goto github
+      </a>
+    </div>
+  )
+}
